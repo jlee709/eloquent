f9fee2926c25191c8e50fb172b72d8f0d7bbaa3b
Justin Wayne Lee <justinlee709@amsesm.cnm7103.co.kr>
2017-08-15 19:51:55 +0900

    pyramid as function to console added

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+/* pyramid example stand alone */
+
+for (var i = 0; i < 10; i++) {
+ console.log(build += '#');
+ }
+
+
+/* Half pyramid example as a function */
+
+function pyr(){
+     build = '';
+for (var i = 0; i < 10; i++) {
+ console.log(build += '#');
+ }
+}
